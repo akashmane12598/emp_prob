87e84c47bd66c74e74feb0d97569353a890896c4
Akash <akashmane12598@gmail.com>
2020-09-25 18:46:49 +0530

    [Akash] Add. Main code usecase1

--- Pracs2/Program.cs
+++ Pracs2/Program.cs
@@ -6,7 +6,21 @@ namespace Pracs2
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee attendance");
+          
+            Console.WriteLine("Enter 1 for present and 0 for absent");
+            Random random = new Random();
+            //int n = Convert.ToInt32(Console.ReadLine());
+            int n = random.Next(0, 2);
+            String att = "";
+            if (n == 1)
+            {
+                att += "Present";
+            }
+            else
+            {
+                att += "Absent";
+            }
+            Console.WriteLine(att);
 
         }
     }
