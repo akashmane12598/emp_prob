3e51546264076919e0dc54c3c1ee29a127f0f6ef
Akash <akashmane12598@gmail.com>
2020-09-26 10:33:47 +0530

    [Akash]Add. Temp usecase4

--- Pracs2/Program.cs
+++ Pracs2/Program.cs
@@ -5,30 +5,25 @@ namespace Pracs2
     class Program
     {
         public static void Main(string[] args)
-        {
-          
-            Console.WriteLine("Enter 1 for present and 0 for absent");
+        { 
             Random random = new Random();
-            //int n = Convert.ToInt32(Console.ReadLine());
-            int n = random.Next(0, 2);
-            String att = "";
-            if (n == 1)
-            {
-                att += "Present";
-            }
-            else
-            {
-                att += "Absent";
-            }
-            Console.WriteLine(att);
+
             int wages_per_hour = 20;
             int hours = 0;
             int wages = 0;
             
-            //int n1 = random.Next(0, 2);
-            if (n == 1)
+            int n = random.Next(0, 2);
+            switch (n)
             {
-                hours = 8;
+                case 0:
+                    hours = 0;
+                    break;
+                case 1:
+                    hours = 8;
+                    break;
+                case 2:
+                    hours = 4;
+                    break;
             }
             wages = hours * wages_per_hour;
             Console.WriteLine("Total wages of an employee: "+wages);
