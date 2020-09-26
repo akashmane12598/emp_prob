0e4a740f063ba49487d7e5302a450ea210583a17
Akash <akashmane12598@gmail.com>
2020-09-26 10:12:47 +0530

    [Akash]Add. temporary usecase3

--- Pracs2/Program.cs
+++ Pracs2/Program.cs
@@ -6,29 +6,19 @@ namespace Pracs2
     {
         public static void Main(string[] args)
         {
-          
-            Console.WriteLine("Enter 1 for present and 0 for absent");
             Random random = new Random();
-            //int n = Convert.ToInt32(Console.ReadLine());
             int n = random.Next(0, 2);
-            String att = "";
-            if (n == 1)
-            {
-                att += "Present";
-            }
-            else
-            {
-                att += "Absent";
-            }
-            Console.WriteLine(att);
             int wages_per_hour = 20;
             int hours = 0;
             int wages = 0;
-            
-            //int n1 = random.Next(0, 2);
+     
             if (n == 1)
             {
                 hours = 8;
+            }
+            else if (n == 2)
+            {
+                hours = 4;
             }
             wages = hours * wages_per_hour;
             Console.WriteLine("Total wages of an employee: "+wages);
