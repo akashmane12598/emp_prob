c41475f63bc2dca661ef4b9d28e49ecf265ca8f4
Akash <akashmane12598@gmail.com>
2020-09-26 18:49:22 +0530

    [Akash]Add. temp usecase3

--- Pracs2/Program.cs
+++ Pracs2/Program.cs
@@ -7,16 +7,16 @@ namespace Pracs2
         public static void Main(string[] args)
         {
             Random random = new Random();
-            int n = random.Next(0, 2);
+            int emp_rec = random.Next(0, 3);
             int wages_per_hour = 20;
             int hours = 0;
             int wages = 0;
      
-            if (n == 1)
+            if (emp_rec == 1)
             {
                 hours = 8;
             }
-            else if (n == 2)
+            else if (emp_rec == 2)
             {
                 hours = 4;
             }
