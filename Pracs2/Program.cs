1715ecd5c92a10d543c4bc65c6ea17f6e46a5f8c
Akash <akashmane12598@gmail.com>
2020-09-28 01:51:54 +0530

    [Akash]Add. Temporary UC3

--- Pracs2/Program.cs
+++ Pracs2/Program.cs
@@ -7,16 +7,16 @@ namespace Pracs2
         public static void Main(string[] args)
         {
             Random random = new Random();
-            int emp_rec = random.Next(0, 3);
+            int n = random.Next(0, 3);
             int wages_per_hour = 20;
             int hours = 0;
             int wages = 0;
      
-            if (emp_rec == 1)
+            if (n == 1)
             {
                 hours = 8;
             }
-            else if (emp_rec == 2)
+            else if (n == 2)
             {
                 hours = 4;
             }
