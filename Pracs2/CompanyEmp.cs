0863040fc9c6e2802663f86abf133c90e2060ac8
Akash <akashmane12598@gmail.com>
2020-09-29 22:02:59 +0530

    [Akash] Add. Temporary Interface Implementation UC15

---
+++ Pracs2/CompanyEmp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pracs2
+{
+    class CompanyEmp
+    {
+        public string company_name;
+        public int emp_wages;
+        public int total_days;
+        public int max_hours;
+        public int totalWages;
+
+        public CompanyEmp(string company_name, int emp_wages, int total_days, int max_hours)
+        {
+            this.company_name = company_name;
+            this.emp_wages = emp_wages;
+            this.total_days = total_days;
+            this.max_hours = max_hours;
+        }
+        public void setTotalWages(int totalWages)
+        {
+            this.totalWages = totalWages;
+        }
+
+    }
+}
